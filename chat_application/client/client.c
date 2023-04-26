87d984f2b9d8a9107bd9e450d2042f37a8e81602
dev nirwal <devnirwal16@gmail.com>
2023-04-26 13:44:44 +0530

    added client code

--- chat_application/client/client.c
+++ chat_application/client/client.c
@@ -7,16 +7,63 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-void error(const char *msg){
+void error(const char *msg)
+{
     perror(msg);
     exit(1);
 }
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     int sockfd, port, n;
     struct sockaddr_in server_addr;
     struct hostent *server;
-    if(argc<3){
+    if (argc < 3)
+    {
         printf("Please provide port number.\n");
-        exit(1);
+        exit(0);
     }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("Error opening socket.");
+    }
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL)
+    {
+        printf("Error, no such host.\n");
+        exit(0);
+    }
+    bzero((char *)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, (char *)&server_addr.sin_addr.s_addr, server->h_length);
+    server_addr.sin_port = htons(portno);
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        error("Connection failed.");
+    }
+    while (1)
+    {
+        bzero(buffer, 255);
+        fgets(buffer, 255, stdin);
+        n = write(sockfd, buffer, strlen(buffer));
+        if (n < 0)
+        {
+            error("Error on writing.");
+        }
+        bzero(buffer, 255);
+        n = read(sockfd, buffer, 255);
+        if (n < 0)
+        {
+            error("Error on reading.");
+        }
+        printf("Server: %s\n", buffer);
+        int i = strncmp("Bye", buffer, 3);
+        if (i == 0)
+        {
+            break;
+        }
+    }
+    close(sockfd);
 }
